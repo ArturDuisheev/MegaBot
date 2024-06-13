365cedf65ccd4f966206af0671dac903d1bc8b1f
GGduisheevArturGG <107888994+ArturDuisheev@users.noreply.github.com>
2024-06-13 20:31:21 +0600

    add firsts scripts

--- scripts/get_info_os.ps1
+++ scripts/get_info_os.ps1
@@ -1,7 +1,53 @@
-Get-CimInstance -ClassName Win32_Desktop
+class UserData {
+    [string]$Name
+    
+    UserData([string]$name) {
+        $this.Name = $name
+    }
 
-Get-CimInstance -ClassName Win32_Desktop | Select-Object -ExcludeProperty "CIM*"
+    [string] GetUserInfo() {
+        return "Name Of System: $($this.Name)"
+    }
+}
 
-Get-CimInstance -ClassName Win32_BIOS
+class UserDataFetcher {
+    [UserData[]] $WinInformation
+    
+    UserDataFetcher() {
+        $this.WinInformation = @()
+        $WinInstances = Get-CimInstance -ClassName Win32_Desktop
+        foreach ($winInstance in $WinInstances) {
+            $userData = [UserData]::new($winInstance.Name)
+            $this.WinInformation += $userData
+        }
+    }
+    
+    [UserData[]] GetAllDataForUser() {
+        return $this.WinInformation
+    }
+}
 
-Get-CimInstance -ClassName Win32_Processor | Select-Object -ExcludeProperty "CIM*"
+class ScriptStarter {
+    [UserDataFetcher] $userDataFetcher
+    
+    ScriptStarter() {
+        $this.userDataFetcher = [UserDataFetcher]::new()
+    }
+    
+    [void] Start() {
+        # $this.SetExecutionPolicy()
+        $userData = $this.userDataFetcher.GetAllDataForUser()
+        foreach ($data in $userData) {
+            Write-Output $data.GetUserInfo()
+        }
+    }
+    
+    # [void] SetExecutionPolicy() {
+    #     Set-ExecutionPolicy Restricted
+    #     Set-ExecutionPolicy AllSigned
+    #     Set-ExecutionPolicy RemoteSigned
+    # }
+}
+
+$starter = [ScriptStarter]::new()
+$starter.Start()
